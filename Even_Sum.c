0452ff5f45447a8ce85a7184db3a32c3fb89ea90
Mastermindmani88897 <147472580+Mastermindmani88897@users.noreply.github.com>
2024-02-20 09:41:03 +0530

    #include<stdio.h> int main(){     int n,i,j,res = 0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++){         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++){         if(arr[i]%2==0)         res = res + arr[i];     }     printf("%d",res);
    }

---
+++ Even_Sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,i,j,res = 0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++){
+        if(arr[i]%2==0)
+        res = res + arr[i];
+    }
+    printf("%d",res);
+    
+}
