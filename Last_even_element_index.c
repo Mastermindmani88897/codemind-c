be7b47ac87f0e34eec5fcc922ae5876e896414b4
Mastermindmani88897 <147472580+Mastermindmani88897@users.noreply.github.com>
2024-02-21 09:36:52 +0530

    #include<stdio.h> int main(){     int n,i,j;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++){         scanf("%d",&arr[i]);     }for(j=n-1;j>=0;j--){         if(arr[j]%2==0){         printf("%d",j);         break;         }     } }

---
+++ Last_even_element_index.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }for(j=n-1;j>=0;j--){
+        if(arr[j]%2==0){
+        printf("%d",j);
+        break;
+        }
+    }
+}
